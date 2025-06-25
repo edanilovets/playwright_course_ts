0fc63df31cd8e4167a3175f513e383b31b1c5160
Eugene Danilovets <eugene.danilovets@gmail.com>
2025-06-25 15:37:58 +0300

    Refactor NavigationPage class to use locators for menu items

--- page-objects/navigation.page.ts
+++ page-objects/navigation.page.ts
@@ -1,14 +1,20 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 export class NavigationPage {
   readonly page: Page;
+  // Locators
+  readonly formsMenuItem: Locator;
+  readonly formLayoutsMenuItem: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    // Initialize locators
+    this.formsMenuItem = page.getByText('Forms');
+    this.formLayoutsMenuItem = page.getByText('Form Layouts');
   }
 
   async openFormLayouts() {
-    await this.page.getByText('Forms').click();
-    await this.page.getByText('Form Layouts').click();
+    await this.formsMenuItem.click();
+    await this.formLayoutsMenuItem.click();
   }
 }
