398a662a7e84378844b48af79a0f1825c62be660
Eugene Danilovets <eugene.danilovets@gmail.com>
2025-06-26 17:22:59 +0300

    Enhance API tests by mocking articles response and verifying article details

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -7,10 +7,26 @@ test.beforeEach(async ({ page }) => {
       body: JSON.stringify(tags),
     });
   });
+
+  // Mocking the articles API response
+  // This will intercept the request to the articles API and return a mocked response
+  await page.route('**/api/articles*', async (route) => {
+    const response = await route.fetch();
+    const data = await response.json();
+    data.articles[0].title = 'Mocked Article Title';
+    data.articles[0].description = 'Mocked Article Description';
+
+    await route.fulfill({
+      body: JSON.stringify(data),
+    });
+  });
+
   await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('Has brand', async ({ page }) => {
-  await page.waitForTimeout(3000);
+  // await page.waitForTimeout(3000);
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toHaveText('Mocked Article Title');
+  await expect(page.locator('app-article-list p').first()).toHaveText('Mocked Article Description');
 });
