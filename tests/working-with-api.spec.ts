b63c0a9dec2f8f10d10084d31ef1fb4b82dc4b5b
Eugene Danilovets <eugene.danilovets@gmail.com>
2025-06-28 00:15:26 +0300

    Refactor API tests to improve structure and clarity by consolidating setup and removing redundant code

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -2,14 +2,18 @@ import { test, expect } from '@playwright/test';
 import tags from '../data/mocks/tags.json';
 
 test.beforeEach(async ({ page }) => {
+  // Mocking the tags API response
   await page.route('**/api/tags', async (route) => {
     await route.fulfill({
       body: JSON.stringify(tags),
     });
   });
 
+  await page.goto('https://conduit.bondaracademy.com/');
+});
+
+test('Has brand', async ({ page }) => {
   // Mocking the articles API response
-  // This will intercept the request to the articles API and return a mocked response
   await page.route('**/api/articles*', async (route) => {
     const response = await route.fetch();
     const data = await response.json();
@@ -21,11 +25,7 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
-  await page.goto('https://conduit.bondaracademy.com/');
-});
-
-test('Has brand', async ({ page }) => {
-  // await page.waitForTimeout(3000);
+  await page.getByText('Global Feed').click();
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
   await expect(page.locator('app-article-list h1').first()).toHaveText('Mocked Article Title');
   await expect(page.locator('app-article-list p').first()).toHaveText('Mocked Article Description');
