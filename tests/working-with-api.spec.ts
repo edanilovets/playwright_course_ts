cb0a653355d08c22626cd949cfdc599cab2f1c32
Eugene Danilovets <eugene.danilovets@gmail.com>
2025-06-28 00:36:01 +0300

    Add article deletion test with login and creation steps

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import tags from '../data/mocks/tags.json';
 
 test.beforeEach(async ({ page }) => {
@@ -29,4 +29,30 @@ test('Has brand', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
   await expect(page.locator('app-article-list h1').first()).toHaveText('Mocked Article Title');
   await expect(page.locator('app-article-list p').first()).toHaveText('Mocked Article Description');
+});
+
+test('Delete article', async ({ page, request }) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      user: { email: 'eugene@test.com', password: '12345678' },
+    },
+  });
+  const loginData = await response.json();
+  const token = loginData.user.token;
+
+  // Create a new article
+  const createResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data: {
+      article: { title: 'Title 1', description: 'Description 1', body: 'Body text 1', tagList: ['automation'] },
+    },
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+  const createData = await createResponse.json();
+
+  // Assert article creation
+  expect(createResponse.ok()).toBeTruthy();
+
+  // TODO: Add steps to login and delete the article
 });
